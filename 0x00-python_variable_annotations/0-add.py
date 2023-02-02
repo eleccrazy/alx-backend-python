eb73a7a498641e3435f26d2923f87427aaf1dfe0
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:20:03 +0300

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+File: 0-add.py
+Desc: This file contains a single function which adds two float
+      numbers and it uses type annotations.
+Author: Gizachew Baynenss
+Date reated: Feb 2, 2023
+"""
+
+
+def add(a: float, b: float) -> float:
+    """This function adds two floating point numbers and returns
+    their result as a floating point number"""
+    return a + b
