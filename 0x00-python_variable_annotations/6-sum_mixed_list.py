86d6d80bf0f395c747476f518ecc4a6c55b399b7
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:32:19 +0300

    fix by casting the result to float

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -12,4 +12,4 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
     """Returns the sum of the numbers in the list as afloat"""
-    return sum(mxd_lst)
+    return float(sum(mxd_lst))
