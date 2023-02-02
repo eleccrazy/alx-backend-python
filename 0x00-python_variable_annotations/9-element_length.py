d70cbe7a7f40f053845748f3cd8573a69bf1b246
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:39:01 +0300

    task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+File: 9-element_length.py
+Desc: This python module contains a function with some params and
+      returns values with the appropriate types.
+Author: Gizachew Bayness
+Date Created: Feb 2, 2023
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns values with appropriate types"""
+    return [(i, len(i)) for i in lst]
