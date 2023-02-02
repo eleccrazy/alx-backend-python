3da18c1bc244b77ae0de65e58d341526424c7681
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:43:17 +0300

    task 11

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+File: 101-safely_get_value.py
+Desc: This python module contains a pre-written function that needs to be
+      typed with python typea annotations.
+Author: Gizachew Bayness
+Date Created: Feb 2, 2023
+"""
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar("T")
+
+
+def safely_get_value(dct: Mapping, key: Any,
+        default=None: Union[T, None]) -> Union[Any, T]:
+    """Type Anotated function"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
