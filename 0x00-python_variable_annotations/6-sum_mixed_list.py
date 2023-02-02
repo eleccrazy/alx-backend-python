02538449f368c05cb428e2bd2ccb2a92dd3071a0
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:25:04 +0300

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+File: 6-sum_mixed_list.py
+Desc: This python module contains a type-annotated function which
+      takes a list of integers and floats and returns their sum
+      as a float.
+Author: Gizachew Bayness
+Date Created: Feb 2, 2023
+"""
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: Union[int | float]) -> float:
+    """Returns the sum of the numbers in the list as afloat"""
+    return sum(mxd_lst)
