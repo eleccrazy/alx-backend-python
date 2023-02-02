bda845b9b4d995920f4a991388e7f6a12bf2b31a
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:23:48 +0300

    task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+File: 4-define_variables.py
+Desc: This python module defines 4 variables with type annotations.
+Author: Gizachew Bayness
+Date Created: Feb 2, 2023
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
