1c8410c69897ebbd3ca77d391b58bf8713011019
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:21:02 +0300

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/ python3
+"""
+File: 1-concat.py
+Desc: This python module contains a single type-annotated funtion
+      that conccatenates two strings.
+Author: Gizachew Bayness
+Date Created: Feb 2, 2023
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the concatenated str"""
+    return str1 + str2
