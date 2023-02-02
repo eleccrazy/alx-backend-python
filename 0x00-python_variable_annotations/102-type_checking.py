556d5ccf5393d988517319ce18a8c59aec68ac09
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:45:23 +0300

    task 12

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+File: 102-type_checking.py
+Desc: This python module contains a pre-written function to be modified with necessary changes
+      and to be validated using mypy.
+Author: Gizachew Bayness
+Date Created: Feb 2, 2023
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """Type checked function"""
+    zoomed_in: Tuple = [
+            item for item in lst
+            for i in range(factor)
+            ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+zoom_2x = zoom_array(array)
+zoom_3x = zoom_array(array, 3.0)
