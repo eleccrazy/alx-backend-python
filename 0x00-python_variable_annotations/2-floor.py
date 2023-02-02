249f5c7cebd502771f9b50be65469e02855a9f33
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:21:41 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+File: 2-floor.py
+Desc: This python module contains a single type-annotated funciton
+      that takes a float n as argument and returns the floor of the
+      float.
+Author: Gizachew Bayness
+Date Created: Feb 2, 2023
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns a floor of a floating number n"""
+    return math.floor(n)
