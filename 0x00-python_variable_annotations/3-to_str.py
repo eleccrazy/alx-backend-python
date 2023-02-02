567069b5068ce4660a7b682ae2ded094a9b5c60b
Gizachew Bayness <eleccrazy24@gmail.com>
2023-02-02 20:23:17 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+File: 3-to_str.py
+Desc: This python module contains a function which takes a flot
+      n as an argument and returns the string representation of
+      the float.
+Author: Gizachew Bayness
+Date Created: Feb 2, 2023
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float"""
+    return str(n)
